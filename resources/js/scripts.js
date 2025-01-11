e47d94e044228531c6d0698b4b3e36eb0320b268
romrauq <johnequarmor@gmail.com>
2025-01-11 10:24:11 +0000

    Removed the display of selected elements by default.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -8,3 +8,14 @@ const email_error = document.getElementById("email-error");
 const password_input = document.getElementById("password-input");
 const password_error = document.getElementById("password-error");
 const submit_button = document.getElementById("submit-button");
+
+// Hide error texts by default:
+first_name_error.style.display = "none";
+last_name_error.style.display = "none";
+email_error.style.display = "none";
+password_error.style.display = "none";
+
+// Hide error icons within input fields by default:
+for (let i = 0; i < input_fields.length; i++) {
+	input_fields[i].style.backgroundImage = "none";
+}
