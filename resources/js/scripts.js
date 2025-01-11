de8371426ff88bbd39ce0fa539bc30fb90f95d9c
romrauq <johnequarmor@gmail.com>
2025-01-11 16:25:16 +0000

    Defined scripts to validate input values and display elements respectively.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -19,3 +19,62 @@ password_error.style.display = "none";
 for (let i = 0; i < input_fields.length; i++) {
 	input_fields[i].style.backgroundImage = "none";
 }
+
+// Function to validate email
+function validateEmail(email) {
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return emailRegex.test(email);
+}
+
+// Add a click event listener to the submit button:
+submit_button.addEventListener("click", (event) => {
+	event.preventDefault();
+
+	// Check if first name input field is empty:
+	if (first_name_input.value == "") {
+		first_name_input.style.border = "2px solid hsl(0, 100%, 74%)";
+		first_name_input.style.backgroundImage = `url("./resources/images/icon-error.svg")`;
+		first_name_error.style.display = "block";
+	} else {
+		first_name_input.style.border = "1px solid hsl(246, 25%, 77%)";
+		first_name_input.style.backgroundImage = "none";
+		first_name_error.style.display = "none";
+	}
+
+	// Check if last name input field is empty:
+	if (last_name_input.value == "") {
+		last_name_input.style.border = "2px solid hsl(0, 100%, 74%)";
+		last_name_input.style.backgroundImage = `url("./resources/images/icon-error.svg")`;
+		last_name_error.style.display = "block";
+	} else {
+		last_name_input.style.border = "1px solid hsl(246, 25%, 77%)";
+		last_name_input.style.backgroundImage = "none";
+		last_name_error.style.display = "none";
+	}
+
+	// Validate email input:
+	const email_value = email_input.value.trim();
+
+	if (validateEmail(email_value)) {
+		email_input.style.border = "1px solid hsl(246, 25%, 77%)";
+		email_input.style.color = "hsl(0, 0.00%, 0.00%)";
+		email_input.style.backgroundImage = "none";
+		email_error.style.display = "none";
+	} else {
+		email_input.style.border = "2px solid hsl(0, 100%, 74%)";
+		email_input.style.color = "hsl(0, 100%, 74%)";
+		email_input.style.backgroundImage = `url("./resources/images/icon-error.svg")`;
+		email_error.style.display = "block";
+	}
+
+	// Check if password input field is empty:
+	if (password_input.value == "") {
+		password_input.style.border = "2px solid hsl(0, 100%, 74%)";
+		password_input.style.backgroundImage = `url("./resources/images/icon-error.svg")`;
+		password_error.style.display = "block";
+	} else {
+		password_input.style.border = "1px solid hsl(246, 25%, 77%)";
+		password_input.style.backgroundImage = "none";
+		password_error.style.display = "none";
+	}
+});
